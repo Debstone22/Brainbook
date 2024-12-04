b245e758ee4a8ea24f39c268ec7f0e59b3f7d1af
Debstone22 <jadasane82@gmail.com>
2024-12-04 12:42:07 -0500

    Cositas

--- config/Database.php
+++ config/Database.php
@@ -1,7 +1,7 @@
 <?php
 class Database {
     private $host = "localhost";
-    private $db_name = "brainbook";
+    private $db_name = "brainbook2";
     private $username = "root";
     private $password = "";
     public $conn;
